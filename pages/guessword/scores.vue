86fa2b802707ea5bac2f48e374597872467747cb
Jeff Rossi <rossi.jeff@gmail.com>
2023-03-26 07:58:13 -0400

    initial scores

--- pages/guessword/scores.vue
+++ pages/guessword/scores.vue
@@ -1,3 +1,13 @@
 <template>
-	<div class="guess-word-scores"></div>
+	<div class="guess-word-scores">{{  state  }}</div>
 </template>
+
+<script lang="ts" setup>
+import { GuessWord } from '~~/utils/types/guess-word.type';
+
+let Items: GuessWord[] = reactive([])
+let state = reactive({ Items, Count: 0, Limit: 10, Offset: 0 })
+const path = '/api/guess_word'
+
+state = await loadScores(path, state.Offset, state.Limit)
+</script>
