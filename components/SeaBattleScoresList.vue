3f58fc814b54f6378fa2eb5cadf194d1b097c958
Jeff Rossi <jrossi@intouchgps.com>
2023-03-28 08:35:54 -0400

    scores list

---
+++ components/SeaBattleScoresList.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="sea-battle-scores-list">
+		<div
+			class="sea-battle-score"
+			v-for="sea_battle of props.items"
+			:key="sea_battle.id"
+		>
+			<div class="link">{{ sea_battle.id }}</div>
+			<div class="status">{{ sea_battle.Status }}</div>
+			<div class="status">{{ sea_battle.Score }}</div>
+			<div class="status">{{ sea_battle.Axis }}</div>
+		</div>
+	</div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps(['items'])
+</script>
+
+<style lang="postcss">
+div.sea-battle-score {
+	@apply flex flex-wrap justify-between mx-2 border border-dashed border-b-gray-500;
+}
+</style>
--- utils/api-url.ts
+++ utils/api-url.ts
@@ -1,2 +1,2 @@
-// export const apiUrl = 'https://games-nest.jeff-rossi.com'
-export const apiUrl = 'http://localhost:4000'
+export const apiUrl = 'https://games-nest.jeff-rossi.com'
+// export const apiUrl = 'http://localhost:4000'
