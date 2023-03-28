066df0e64de8cf767ac2a03206190c2adce2144f
Jeff Rossi <jrossi@intouchgps.com>
2023-03-28 15:12:30 -0400

    hang man scores

---
+++ components/HangManScoresList.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="hang-man-scores-list">
+		<div
+			class="hang-man-score"
+			v-for="hang_man of props.items"
+			:key="hang_man.id"
+		>
+			<div class="link">{{ hang_man.id }}</div>
+			<div class="status">{{ hang_man.Status }}</div>
+			<div class="score">{{ hang_man.Score }}</div>
+			<div class="word">{{ hang_man.word ? hang_man.word.Word : '' }}</div>
+			<div class="length">
+				{{ hang_man.word ? hang_man.word.Length : 0 }}
+			</div>
+		</div>
+	</div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps(['items'])
+</script>
+
+<style lang="postcss">
+div.hang-man-score {
+	@apply flex flex-wrap justify-between mx-2 border border-dashed border-b-gray-500;
+}
+</style>
