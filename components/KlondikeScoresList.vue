cbe40e760933839dc501926e99a562f63308813d
Jeff Rossi <rossi.jeff@gmail.com>
2023-07-09 20:14:03 -0400

    gameplay and scores

--- components/KlondikeScoresList.vue
+++ components/KlondikeScoresList.vue
@@ -1,48 +1,48 @@
 <template>
-	<div class="klondike-scores-list">
-		<div class="free-cell-header">
-			<div class="k-user">User</div>
-			<div class="status">Status</div>
-			<div class="time">Time</div>
-			<div class="moves">Moves</div>
-		</div>
-		<div
-			class="klondike-score"
-			v-for="klondike of props.items"
-			:key="klondike.id"
-		>
-			<div class="k-user">
-				{{ klondike.user ? klondike.user.UserName : 'Anonymous' }}
-			</div>
-			<div class="status">{{ klondike.Status }}</div>
-			<div class="time">{{ formatElapsedSeconds(klondike.Elapsed) }}</div>
-			<div class="moves">{{ klondike.Moves }}</div>
-		</div>
-	</div>
+  <div class="klondike-scores-list">
+    <div class="klondike-header">
+      <div class="k-user">User</div>
+      <div class="status">Status</div>
+      <div class="time">Time</div>
+      <div class="moves">Moves</div>
+    </div>
+    <div
+      class="klondike-score"
+      v-for="klondike of props.items"
+      :key="klondike.id"
+    >
+      <div class="k-user">
+        {{ klondike.user ? klondike.user.UserName : "Anonymous" }}
+      </div>
+      <div class="status">{{ klondike.Status }}</div>
+      <div class="time">{{ formatElapsedSeconds(klondike.Elapsed) }}</div>
+      <div class="moves">{{ klondike.Moves }}</div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
-const props = defineProps(['items'])
+const props = defineProps(["items"]);
 </script>
 
 <style lang="postcss">
 div.klondike-scores-list {
-	@apply my-2;
+  @apply my-2;
 }
-div.free-cell-header {
-	@apply flex flex-wrap justify-between font-bold bg-gray-200 mb-1 px-1;
+div.klondike-header {
+  @apply flex flex-wrap justify-between font-bold bg-gray-200 mb-1 px-1;
 }
 div.klondike-score {
-	@apply flex flex-wrap justify-between border border-dashed border-b-gray-500;
+  @apply flex flex-wrap justify-between border border-dashed border-b-gray-500;
 }
 div.k-user {
-	@apply w-32 text-left;
+  @apply w-32 text-left;
 }
 div.status,
 div.time {
-	@apply w-20 text-center;
+  @apply w-20 text-center;
 }
 div.moves {
-	@apply w-20 text-right;
+  @apply w-20 text-right;
 }
 </style>
---
+++ components/PokeSquareScoreList.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="poker-squares-scores-list">
+    <div class="poker-squares-header">
+      <div class="ps-user">User</div>
+      <div class="status">Status</div>
+      <div class="ps-score">Score</div>
+    </div>
+    <div
+      class="poker-squares-score"
+      v-for="pokerSquare of props.items"
+      :key="pokerSquare.id"
+    >
+      <div class="ps-user">
+        {{ pokerSquare.user ? pokerSquare.user.UserName : "Anonymous" }}
+      </div>
+      <div class="status">{{ pokerSquare.Status }}</div>
+      <div class="ps-score">{{ pokerSquare.Score }}</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps(["items"]);
+</script>
+
+<style lang="postcss">
+div.poker-squares-scores-list {
+  @apply my-2;
+}
+div.poker-squares-header {
+  @apply flex flex-wrap justify-between font-bold bg-gray-200 mb-1 px-1;
+}
+div.poker-squares-score {
+  @apply flex flex-wrap justify-between border border-dashed border-b-gray-500;
+}
+div.ps-user {
+  @apply w-32 text-left;
+}
+div.status {
+  @apply w-20 text-center;
+}
+div.ps-score {
+  @apply w-20 text-right;
+}
+</style>
